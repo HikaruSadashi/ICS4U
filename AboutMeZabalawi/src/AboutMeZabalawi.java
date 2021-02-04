b7ca9762e97a303d192df03e3cf851fe4ef8fb9e
Khalid <HikaruSadashi@users.noreply.github.com>
2021-02-04 11:48:22 -0500

    Update AboutMeZabalawi.java

--- AboutMeZabalawi/src/AboutMeZabalawi.java
+++ AboutMeZabalawi/src/AboutMeZabalawi.java
@@ -4,8 +4,8 @@ public class AboutMeZabalawi {
         System.out.println("I live in London Ontario.");
         System.out.println("I took the 3U version and found it interesting.");
         System.out.println("I hope to better my proficiency in Java.");
-        System.out.println("My favourite piece of technology would be  ");
-        System.out.println("My favourite to app use is VS Code");
+        System.out.println("My favourite piece of technology would be the the MOP-IT satellite.");
+        System.out.println("My favourite app to use is VS Code");
         System.out.println("I speedrun an IKEA furniture.");
     }
 }
