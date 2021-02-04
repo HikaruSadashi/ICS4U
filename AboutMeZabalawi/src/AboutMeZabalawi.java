e9b327dcd493c65ec08f15cfe821f8224ceec631
Khalid <HikaruSadashi@users.noreply.github.com>
2021-02-04 09:44:51 -0500

    Finish Project

---
+++ AboutMeZabalawi/src/AboutMeZabalawi.java
@@ -0,0 +1,11 @@
+public class AboutMeZabalawi {
+    public static void main(String[] args) throws Exception {
+        System.out.println("My name is Khalid Zabalawi.");
+        System.out.println("I live in London Ontario.");
+        System.out.println("I took the 3U version and found it interesting.");
+        System.out.println("I hope to better my proficiency in Java.");
+        System.out.println("My favourite piece of technology would be  ");
+        System.out.println("My favourite to use is VS Code");
+        System.out.println("I speedrun an IKEA furniture.");
+    }
+}
