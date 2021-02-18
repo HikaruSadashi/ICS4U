8485d5cec8eed2cc665427b50b72ae580ac1d504
Khalid <HikaruSadashi@users.noreply.github.com>
2021-02-17 19:13:30 -0500

    Added review question 1

---
+++ ReviewActivity1/src/ReviewActivity1.java
@@ -0,0 +1,25 @@
+public class ReviewActivity1 {
+    public static void main(String[] args) throws Exception {
+        //assigning variables
+        String name;
+        String funfact;
+        int age;
+        double height;
+        boolean petfriendly;
+
+        //assigning values to said variables
+        name = "Khalid";
+        funfact = "Likes to play volleyball";
+        age = 17;
+        height = 6.0;
+        petfriendly = true;
+
+        //printing the variables alongside string desciption
+        System.out.println("Name: " + name);
+        System.out.println("A funfact about them is: " + funfact);
+        System.out.println("Age:  " + age);
+        System.out.println("Height: " + height + "ft");
+        System.out.println("They are friendly with pets: " + petfriendly);
+
+    }
+}
