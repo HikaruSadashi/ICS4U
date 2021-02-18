9d9eb0f17d020591149d639e82914c986165e83f
Khalid <HikaruSadashi@users.noreply.github.com>
2021-02-17 19:28:10 -0500

    Review Activity 2

---
+++ ReviewActivity2/src/ReviewActivity2.java
@@ -0,0 +1,36 @@
+import java.io.*;
+public class ReviewActivity2 {
+    public static void main(String[] args) throws Exception {
+        DataInputStream input = new DataInputStream(System.in);
+
+        // assigning values
+        int heightOfPrism, lengthOfPrism, widthOfPrism;
+        String units = "???";
+
+        // introducing the user
+        System.out.println("Welcome to RectangularPrism Calculator4000!");
+        System.out.println();
+
+        // prompting the user
+
+        System.out.print("Enter the units of measurement you will be using for all values: ");
+        units = input.readLine();
+
+        System.out.print("Enter the height of the rectangular prism (no decimals please): ");
+        heightOfPrism = Integer.parseInt(input.readLine());
+
+        System.out.print("Enter the length of the rectangular prism (no decimals please): ");
+        lengthOfPrism = Integer.parseInt(input.readLine());
+
+        System.out.print("Enter the width of the rectangular prism (no decimals please): ");
+        widthOfPrism = Integer.parseInt(input.readLine());
+
+        // calculating and assigning volume
+        int volumeOfPrism = heightOfPrism*lengthOfPrism*widthOfPrism;
+
+        // printing volume
+        System.out.println();
+        System.out.print("The volume of the rectangular prism is: " + volumeOfPrism + " " + units);
+
+    }
+}
