a83ddfcff75220931aa1c92b08636ebc3b2e631c
Khalid Zabalawi <58151955+HikaruSadashi@users.noreply.github.com>
2021-02-19 09:29:37 -0500

    Update ReviewActivity1.java

--- ReviewActivity1/src/ReviewActivity1.java
+++ ReviewActivity1/src/ReviewActivity1.java
@@ -15,11 +15,12 @@ public class ReviewActivity1 {
         petfriendly = true;
 
         //printing the variables alongside string desciption
-        System.out.println("Name: " + name);
-        System.out.println("A funfact about them is: " + funfact);
-        System.out.println("Age:  " + age + " year(s)");
-        System.out.println("Height: " + height + "ft");
+        System.out.println("Name:                        " + name);
+        System.out.println("A funfact about them is:     " + funfact);
+        System.out.println("Age:                         " + age + " year(s)");
+        System.out.println("Height:                      " + height + "ft");
         System.out.println("They are friendly with pets: " + petfriendly);
-
+        
+        //delete; check vars and null sets
     }
 }
