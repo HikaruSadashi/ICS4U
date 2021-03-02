89e11c3ac4b48bcb653022a5a30422ecaec99ec0
Khalid Zabalawi <58151955+HikaruSadashi@users.noreply.github.com>
2021-03-02 09:56:55 -0500

    Final

--- File2/src/File2Program.java
+++ File2/src/File2Program.java
@@ -7,17 +7,13 @@ public class File2Program {
         BufferedReader rF = new BufferedReader (new FileReader("c://Users//Khalid Zabalawi//Documents//GitHub//ICS4U//File2//file2.txt"));
 
         for (int i = 1; i <= 5; i++) {
+        
             firstName = rF.readLine();
-            System.out.print(firstName + "\t");
-
             lastName = rF.readLine();
-            System.out.print(lastName + "\t");
-
             phoneNumber = rF.readLine();
-            System.out.print(phoneNumber + "\t");
-
             age = Integer.parseInt(rF.readLine());
-            System.out.println(age);
+            
+            System.out.println(firstName + "\t" + lastName + "\t" + phoneNumber + "\t");
         }
         rF.close();
     }
