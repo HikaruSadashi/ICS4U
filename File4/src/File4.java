a391663f3467bf232482b4652eac031195fea806
Khalid Zabalawi <58151955+HikaruSadashi@users.noreply.github.com>
2021-03-02 11:56:26 -0500

    Update File4.java

--- File4/src/File4.java
+++ File4/src/File4.java
@@ -4,8 +4,8 @@ public class File4 {
     public static void main(String[] args) throws IOException {
         //lastnames, fisrt names, hours, rate, gross pay, total pay roll
         String lastName, firstName;
-        int hours, overTime=0;
-        double rate, grossPay = 0.00, totalPayroll = 0.00;
+        int hours;
+        double rate, grossPay = 0.00, totalPayroll = 0.00, overTime = 0.00;
 
         BufferedReader rF = new BufferedReader (new FileReader("file4.txt"));
         NumberFormat d = new DecimalFormat ("0.00");
@@ -19,11 +19,14 @@ public class File4 {
             lastName = rF.readLine();
             firstName = rF.readLine();
             hours = Integer.parseInt(rF.readLine());
-            if (hours >= 40) {
-                overTime = hours - 40;
-            }
             rate = Double.parseDouble(rF.readLine());
-            grossPay = (rate*(hours-overTime))+(overTime*(1.50*rate));
+            if (hours >= 40) {
+                overTime = hours - 40.00;
+                grossPay = ((rate*40)+(overTime*1.5*rate));
+            }
+            else {
+                grossPay = (rate*hours);
+            }
             
             System.out.println(lastName + "\t" + firstName + "\t" + hours + "\t" + d.format(rate) + "\t" + d.format(grossPay));
 
