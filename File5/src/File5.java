c020f2a690365a9d3da7d4dbd7c1a7a8e81facbd
Khalid Zabalawi <58151955+HikaruSadashi@users.noreply.github.com>
2021-03-02 11:08:39 -0500

    Update File5.java

--- File5/src/File5.java
+++ File5/src/File5.java
@@ -1,5 +1,5 @@
-import java.io.*;
-//add decimal formatting library and example
+\import java.io.*;
+import java.text.*;
 public class File5 {
     public static void main(String[] args) throws Exception {
         String lastName, firstName, gender;
@@ -7,9 +7,9 @@ public class File5 {
         double average, classAvg = 0.0;
         
         BufferedReader rF = new BufferedReader (new FileReader("students.txt"));
+        NumberFormat d = new DecimalFormat ("0.00");
 
         System.out.println("Last" + "\t" + "First" + "\t" + "Math" + "\t" + "English" + "\t" + "Average");
-        //1 output is not aligned
         System.out.println();
 
         for (int i= 1; i <= 11; i++) {
@@ -27,8 +27,6 @@ public class File5 {
             engAvg = Integer.parseInt(rF.readLine());
 
             average = ((engAvg + mathAvg)/2.0);
-            //why divide by 2.0 instead?
-            //decimal format this to 1 extra if I want
             classAvg = classAvg + average;
             
             System.out.println(lastName + "\t" + firstName + "\t" + mathAvg + "\t" + engAvg + "\t" + average);
@@ -37,6 +35,6 @@ public class File5 {
         classAvg = (classAvg/11.0);
         //decimal format this.
         System.out.println();
-        System.out.println("Class Avg" + "\t" + "\t" + "\t" + classAvg);
+        System.out.println("Class Avg" + "\t" + "\t" + "\t" + d.format(classAvg));
     }
 }
