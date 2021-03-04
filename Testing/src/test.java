43b7dba93b1178715e882e5d67d4e5cf5cb6eacb
Khalid <HikaruSadashi@users.noreply.github.com>
2021-03-04 14:46:39 -0500

    Update

---
+++ Testing/src/test.java
@@ -0,0 +1,8 @@
+import org.graalvm.compiler.lir.Variable;
+
+public class test {
+    public static void main(String[] args) throws Exception {
+        String password = "monkey";
+        password.length();
+    }
+}
